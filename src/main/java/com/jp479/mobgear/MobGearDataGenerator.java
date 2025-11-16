79a888bfb17add5db85352a501ded99a956dfbe1
jp479 <jonathanpablo479@gmail.com>
2025-11-16 08:25:19 -0800

    Initial commit

---
+++ src/main/java/com/jp479/mobgear/MobGearDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jp479.mobgear;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MobGearDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
