ad29c7567fee2f5138309e1df9de105397b39e65
jp479 <153343826+jp479@users.noreply.github.com>
2025-11-22 21:58:30 -0800

    Delete remappedSrc/com/jp479/mobgear directory

--- remappedSrc/com/jp479/mobgear/item/custom/BlazePickaxe.java
+++
@@ -1,9 +0,0 @@
-package com.jp479.mobgear.item.custom;
-
-import net.minecraft.item.Item;
-
-public class BlazePickaxe extends Item {
-    public BlazePickaxe(net.minecraft.item.Item.Settings settings) {
-        super(settings);
-    }
-}
